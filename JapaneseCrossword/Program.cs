693f932aaeaa88db3e06662db71b49789c5710d7
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-03-08 15:45:42 +0500

    Тесты вынесены в отдельный проект. Добавлено считывание входных аргументов в Main.

--- JapaneseCrossword/Program.cs
+++ JapaneseCrossword/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace JapaneseCrossword
+﻿using System;
+using System.IO;
+
+namespace JapaneseCrossword
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: \t JapaneseCrossword.exe inputFileName outputFileName");
+                
+                Environment.Exit(0);
+            }
+            
+            var inputFileName = args[0];
+            var outputFileName = args[1];
+
+            if (!File.Exists(inputFileName))
+            {
+                Console.WriteLine("No input file.");
+
+                Environment.Exit(0);
+            }
         }
     }
 }
