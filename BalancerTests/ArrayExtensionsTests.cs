a2f5cc55b82be151185f2d786f5212a8da20f9d1
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-03-27 18:29:08 +0500

    Добавлены тесты на перемешивание массива.

---
+++ BalancerTests/ArrayExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Balancer;
+using NUnit.Framework;
+
+namespace BalancerTests
+{
+    [TestFixture]
+    public class ArrayExtensionsTests
+    {
+        [TestCase(new []{1, 2, 3})]
+        [TestCase(new int[0])]
+        [TestCase(new []{1})]
+        public void Shuffle_ForRandomArrayReturnsCorrectResult(int[] array)
+        {
+            var mixedArray = array.Shuffle();
+            Assert.That(mixedArray, Is.EquivalentTo(array));
+        }
+    }
+}
